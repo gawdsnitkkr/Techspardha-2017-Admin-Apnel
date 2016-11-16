904ec5bf7a46cdf723074daedbb0926566d34fa3
nkkumawat <nk0kumawat@gmail.com>
2016-11-16 16:45:31 +0530

    nk

--- app/shared/event.interface.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=event.interface.js.map
--- app/shared/participant.interface.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by varun on 1/11/16.
- */
-"use strict";
-//# sourceMappingURL=participant.interface.js.map
